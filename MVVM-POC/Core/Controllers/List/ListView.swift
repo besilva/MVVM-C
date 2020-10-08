453ae8bd3f38d722e17a20a42f1c96d82d65cc88
Bernardo Silva <silva98.bernardo@gmail.com>
2020-10-08 10:33:33 -0300

    add viewcodable protocol

--- MVVM-POC/Core/Controllers/List/ListView.swift
+++ MVVM-POC/Core/Controllers/List/ListView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ListView: UIView {
-    
+
     private var loader: UIActivityIndicatorView
     private var tableView: UITableView
     internal var action: (Character) -> Void
@@ -18,43 +18,23 @@ class ListView: UIView {
             self.tableView.reloadData()
         }
     }
-    
+
     init(action: @escaping (Character) -> Void) {
         loader = UIActivityIndicatorView(style: .gray)
         tableView = UITableView()
         self.action = action
         super.init(frame: .zero)
-        setupTableView()
-        createLoader()
+        setupView()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        self.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalToSystemSpacingBelow: self.topAnchor, multiplier: 1),
-            tableView.leadingAnchor.constraint(equalTo:self.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalToSystemSpacingBelow: self.bottomAnchor, multiplier: 1)
-        ])
-    }
-    
-    func createLoader() {
-        self.addSubview(loader)
-        loader.hidesWhenStopped = true
-        loader.center = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
-        loader.startAnimating()
     }
     
 }
 
 extension ListView: UITableViewDelegate, UITableViewDataSource {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let viewModel = viewModel else { return 0 }
         return viewModel.count
@@ -73,14 +53,45 @@ extension ListView: UITableViewDelegate, UITableViewDataSource {
         guard let character = viewModel?.getCharacter(id: indexPath.row) else { return }
         action(character)
     }
+
 }
 
 extension ListView: CharacterListViewModelDelegate {
+
     func stopLoading() {
         DispatchQueue.main.async {
             self.loader.stopAnimating()
         }
     }
+
+}
+
+extension ListView: ViewCodable {
     
+    func configure() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func setupHierarchy() {
+        self.addSubview(tableView)
+         self.addSubview(loader)
+    }
+    
+    func setupConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalToSystemSpacingBelow: self.topAnchor, multiplier: 1),
+            tableView.leadingAnchor.constraint(equalTo:self.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalToSystemSpacingBelow: self.bottomAnchor, multiplier: 1)
+        ])
+    }
+    
+    func render() {
+        loader.hidesWhenStopped = true
+        loader.center = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
+        loader.startAnimating()
+    }
     
 }
---
+++ MVVM-POC/Core/Helper/ViewCodable.swift
@@ -0,0 +1,38 @@
+//
+//  ViewCodable.swift
+//  MVVM-POC
+//
+//  Created by Bernardo Silva on 08/10/20.
+//  Copyright © 2020 Bernardo. All rights reserved.
+//
+
+import UIKit
+
+protocol ViewCodable {
+    /// Use to setup delegates and things like delegates and Images from imageViews
+    func configure()
+    /// Use to add any UI components inside the view
+    func setupHierarchy()
+    /// Setup constraints to the view and the subviews
+    func setupConstraints()
+    /// Customize the UIComponents(fonts, colors)
+    func render()
+    /// Setup any acessibility to the view
+    func setupAcessibilityIdentifiers()
+    /// This method builds the view using all the other methods fom the protocol
+    func setupView()
+}
+
+extension ViewCodable {
+    func setupAcessibilityIdentifiers() {}
+    
+    func setupView() {
+        configure()
+        setupHierarchy()
+        setupConstraints()
+        render()
+        setupAcessibilityIdentifiers()
+    }
+    
+    func configure() {}
+}
